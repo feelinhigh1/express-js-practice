46c43b5d585c07e0a44570e706ed57fdc5136fe1
feelinhigh1 <sushan.poudel.55555@gmail.com>
2025-08-19 12:14:09 +0545

    Serving Static FIles

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ import express from "express";
 const app = express();
 const PORT = 8000;
 
-//Set EJS as the view engine
-app.set("view engine", "ejs");
+app.use("/public", express.static("public")); //http://localhost:8000/public/example.txt
+app.use("/images", express.static("images")); //http://localhost:8000/images/abc.jpg
 
 // Define a simple route
 app.get("/", (req, res) => {
-  const userName = "Sushan Poudel";
-  res.render("index", { userName });
+  res.send("Hello Express");
 });
 
 app.listen(PORT, () => {
