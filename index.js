6bc96cc38b4ad131b603824be2a6ef62055e3869
feelinhigh1 <sushan.poudel.55555@gmail.com>
2025-09-08 08:54:46 +0545

    Session Management

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 const PORT = 3000;
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "sample-secret",
+    resave: false, // don't save session if unmodified
+    saveUninitialized: true,
+  })
+);
 
 // Define a simple route
 app.get("/", (req, res) => {
-  res.cookie("myCookie", "cookieValue"); // , { maxAge: 360000 } means expires in 6 minutes
   res.send("Hello Express");
 });
 
-app.get("/fetch", (req, res) => {
-  console.log(req.cookies);
-  res.send("API Called");
+app.get("/visit", (req, res) => {
+  if (req.session.page_views) {
+    req.session.page_views++;
+    res.send(`You visited this page ${req.session.page_views} times`);
+  } else {
+    req.session.page_views = 1;
+    res.send("Welcome to this page for the first time!");
+  }
 });
 
-app.get("/remove-cookie", (req, res) => {
-  res.clearCookie("myCookie");
-  res.send("Cookie Removed");
+app.get("/remove-visit", (req, res) => {
+  req.session.destroy();
+  res.send("Session destroyed");
 });
 
 app.listen(PORT, () => {
