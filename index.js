da2690e9cdd6934f70129ef1d47f4ae7ee1bf04c
feelinhigh1 <sushan.poudel.55555@gmail.com>
2025-09-13 14:42:59 +0545

    Session based Authentication

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import session from "express-session";
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
 app.use(cookieParser());
 app.use(
   session({
@@ -18,19 +20,32 @@ app.get("/", (req, res) => {
   res.send("Hello Express");
 });
 
-app.get("/visit", (req, res) => {
-  if (req.session.page_views) {
-    req.session.page_views++;
-    res.send(`You visited this page ${req.session.page_views} times`);
-  } else {
-    req.session.page_views = 1;
-    res.send("Welcome to this page for the first time!");
-  }
+const users = [];
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  users.push({
+    username,
+    password,
+  });
+  res.send("User registered");
 });
 
-app.get("/remove-visit", (req, res) => {
-  req.session.destroy();
-  res.send("Session destroyed");
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((u) => u.username === username);
+  if (!user || password !== user.password) {
+    return res.send("Invalid credentials");
+  }
+  req.session.user = user;
+  res.send("User Logged In");
+});
+
+app.get("/dashboard", (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).send("Please login to access this page");
+  }
+  res.send(`Welcome to your dashboard, ${req.session.user.username}`);
 });
 
 app.listen(PORT, () => {
